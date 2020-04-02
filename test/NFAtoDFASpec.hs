16ceff581993fa0bef3ad6f779f0e3c62c456de7
Shanedrafahl <sdrafahl@pop-os.localdomain>
2020-04-01 20:23:31 -0500

    Adding another test to cover basic or NFA.

--- test/NFAtoDFASpec.hs
+++ test/NFAtoDFASpec.hs
@@ -15,9 +15,10 @@ import qualified Data.Map as Map
 
 spec :: Spec
 spec = do
+
   
   describe "NFAToDFA" $ do
     it "Should create a basic DFA (a)" $ convert (NFA ["0", "1"] "0" ["1"] [(Transition "0" (Character 'a') "1")]) `shouldBe` (DFA ["0", "1"] "0" ["1"] [(Transition "0" 'a' "1")])
     it "Should create a basic DFA (ab)" $ convert (NFA ["0", "1", "2", "3"] "0" ["3"] [(Transition "1" EmptyChar "2"), (Transition "0" (Character 'a') "1"), (Transition "2" (Character 'b') "3")]) `shouldBe` (DFA ["0", "1", "2"] "0" ["2"] [(Transition "0" 'a' "1"), (Transition "1" 'b' "2")])
-
-  
+    it "Should create a basic DFA (a|b)" $ convert (NFA ["1","2","3","4","5","6"] "1" ["6"] [(Transition "1"  EmptyChar  "2"),(Transition "1"  EmptyChar  "3"),(Transition "2"  (Character 'a')  "4"),(Transition "3"  (Character 'b')  "5"),(Transition "4" EmptyChar "6"),(Transition "5"  EmptyChar  "6")]) `shouldBe` (DFA ["0", "1", "2"] "0" ["1", "2"] [(Transition "0" 'b' "1"), (Transition "0" 'a' "2")])
+    
