76d03075dedb16f1d7c7b91fc0204abb690b1d0d
sdrafahl <shanedrafahl@gmail.com>
2020-04-12 19:32:02 -0500

    Adding another test.

--- test/DFAMinimizationSpec.hs
+++ test/DFAMinimizationSpec.hs
@@ -15,3 +15,4 @@ spec = do
   describe "minimizeDFA" $ do
     it "Should minimize basic DFA (a)" $ minimizeDFA (DFA ["0", "1"] "0" ["1"] [(Transition "0" 'a' "1")]) `shouldBe` (DFA ["0", "1"] "0" ["1"] [(Transition "0" 'a' "1")])
     it "Should minimize DFA (ab)" $ minimizeDFA (DFA ["0", "1", "2"] "0" ["2"] [(Transition "0" 'a' "1"), (Transition "1" 'b' "2")]) `shouldBe` (DFA ["0", "1", "2"] "1" ["2"] [(Transition "1" 'a' "0"), (Transition "0" 'b' "2")])
+    it "Should minimize DFA (a|b)" $ minimizeDFA ((DFA ["0", "1", "2"] "0" ["1", "2"] [(Transition "0" 'b' "1"), (Transition "0" 'a' "2")])) `shouldBe` ((DFA ["0", "1"] "0" ["1"] [(Transition "0" 'b' "1"), (Transition "0" 'a' "1")]))
