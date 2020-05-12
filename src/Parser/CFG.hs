a4130ff63ff8343c6e17c9516bbb49950c5ea811
sdrafahl <shanedrafahl@gmail.com>
2020-05-11 20:52:46 -0500

    Adding a Context free grammar data type.

---
+++ src/Parser/CFG.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+module CFG where
+
+import Data.Set
+
+data NonTerminal = NonTerminal String
+data Terminal = Terminal String
+data NonTerminalOrTerminal = Term Terminal | NonTerm NonTerminal
+data ProductionRule = ProductionRule (NonTerminalOrTerminal, NonTerminalOrTerminal)
+data StartSymbol = StartSymbol String
+
+data CFG = CFG {nonTerminals :: Set NonTerminal, terminals :: Set Terminal, productionRules :: Set ProductionRule, startSymbols :: Set StartSymbol}
