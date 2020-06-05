aa6ab2f0e5da475370d5593cabca209060c5f852
sdrafahl <shanedrafahl@gmail.com>
2020-06-04 22:18:50 -0500

    Wip, completed first terminal mapping creation.

--- src/Parser/First.hs
+++ src/Parser/First.hs
@@ -22,14 +22,19 @@ addMappingToFirst key value (First values) =
         Just existingSet -> Data.Set.insert value existingSet
   in  First (Data.Map.insert key newSetForTheKey values)
 
-getFirst :: First -> NonTerminalOrTerminal -> Maybe (Set Terminal)
-getFirst (First values) tokenKey = Data.Map.lookup tokenKey values
+addFirst :: First -> NonTerminalOrTerminal -> Set Terminal -> First
+addFirst (First values') key values = (First (Data.Map.insert key values values'))
 
-class FoldingAlgorithm dataToFold init algo res where
-  algorithm :: dataToFold -> init -> algo -> res
+getFirst :: First -> NonTerminalOrTerminal -> Set Terminal
+getFirst (First values) tokenKey = case Data.Map.lookup tokenKey values of
+                                     Nothing -> Data.Set.empty
+                                     Just terms -> terms
 
-instance FoldingAlgorithm (Set ProductionRule) First (First -> ProductionRule -> First) First where
-  algorithm productionRules' intiStartMap algo = (Data.Set.foldl' algo intiStartMap productionRules')
+class FoldingAlgorithm dataToFold init algo where
+  foldingAlgorithm :: dataToFold -> init -> algo -> init
+
+instance FoldingAlgorithm (Set ProductionRule) First (First -> ProductionRule -> First) where
+  foldingAlgorithm productionRules' intiStartMap algo = (Data.Set.foldl' algo intiStartMap productionRules')
 
 class FixedPointAlgorithm d where
   fixPointOperation :: d -> (d -> d) -> d
@@ -41,12 +46,41 @@ instance FixedPointAlgorithm First where
       True -> first
       False -> fixPointOperation first' firstOperation
 
--- Main Algorithm                                     
--- createFirstMap :: Set ProductionRule -> Set Terminal -> First
--- createFirstMap setOfProductionRules terminals =
---   let (intialFirst :: First) = (First Data.Map.empty)
---       (intialFirstWithTerminalsSet :: First) = Data.Set.foldl' intialFirst (\first terminal -> addMappingToFirst (Term terminal) terminal first) terminals
---       (intialFirstWithTerminalsSetWithEmptyString :: First) = addMappingToFirst (Term (Terminal "δ")) (Term (Terminal "δ")) intialFirstWithTerminalsSet
---       (intialFirstWithTerminalsSetWithEmptyStringAndEOF :: First) = addMappingToFirst (Term (Terminal "EOF")) (Term (Terminal "EOF")) intialFirstWithTerminalsSet
+
+data Flag = ReachedAllChildren | DidNotReachAllChildren
+addTerminalsToRHS :: Set Terminal -> [NonTerminalOrTerminal] -> First -> (Set Terminal, Flag)
+addTerminalsToRHS rhs [] _ = (rhs, ReachedAllChildren)
+addTerminalsToRHS rhs [_] _ = (rhs, ReachedAllChildren)
+addTerminalsToRHS rhs (x1:(x2:xs)) first
+  | Data.Set.member (Terminal "δ") (getFirst first x1) = addTerminalsToRHS (Data.Set.union rhs (Data.Set.delete (Terminal "δ") (getFirst first x2))) (x2:xs) first
+  | otherwise = (rhs, DidNotReachAllChildren)
       
+
+addTerminalsToFirst :: First -> ProductionRule  -> First
+addTerminalsToFirst first productionRule =
+  let ((x :: NonTerminalOrTerminal):(xs :: [NonTerminalOrTerminal])) = getChildrenFromProductionRule productionRule
+      (rhs :: Set Terminal) = Data.Set.delete (Terminal "δ") (getFirst first x)
+      (rhs' :: Set Terminal, (flag :: Flag)) = addTerminalsToRHS rhs (x:xs) first
+      (rhs'' :: Set Terminal) = case flag of
+                                  ReachedAllChildren -> Data.Set.insert (Terminal "δ") rhs'
+                                  DidNotReachAllChildren -> rhs'
+      (a :: NonTerminalOrTerminal) = getParentFromProductionRule productionRule
+      (rhs''' :: Set Terminal) = Data.Set.union (rhs'') (getFirst first a)
+  in  addFirst first a rhs'''
+
+addTerminalsForEachProduction :: (Set ProductionRule) -> First -> First
+addTerminalsForEachProduction prodRules first = foldingAlgorithm prodRules first addTerminalsToFirst
+
+addTerminalsUntilFixedPoint :: First -> (Set ProductionRule) -> First
+addTerminalsUntilFixedPoint first prodRules = fixPointOperation first fixedPointFirstAlgorithm
+  where (fixedPointFirstAlgorithm :: (First -> First)) = addTerminalsForEachProduction prodRules
+
+-- Main Algorithm                                     
+createFirstMap :: Set ProductionRule -> Set Terminal -> First
+createFirstMap setOfProductionRules terminals =
+  let (intialFirst :: First) = (First Data.Map.empty)
+      (intialFirstWithTerminalsSet :: First) = (Data.Set.foldl' (\first terminal -> addMappingToFirst (Term terminal) terminal first) intialFirst terminals)
+      (intialFirstWithTerminalsSetWithEmptyString :: First) = addMappingToFirst (Term (Terminal "δ")) (Terminal "δ") intialFirstWithTerminalsSet
+      (intialFirstWithTerminalsSetWithEmptyStringAndEOF :: First) = addMappingToFirst (Term (Terminal "EOF")) (Terminal "EOF") intialFirstWithTerminalsSet
+  in  addTerminalsUntilFixedPoint intialFirstWithTerminalsSetWithEmptyStringAndEOF setOfProductionRules
 
