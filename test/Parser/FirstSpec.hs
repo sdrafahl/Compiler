97998cdffc7fd8feacecce3d37372d5431afaac0
sdrafahl <shanedrafahl@gmail.com>
2020-06-17 19:54:10 -0500

    Adding another test to First.

--- test/Parser/FirstSpec.hs
+++ test/Parser/FirstSpec.hs
@@ -21,11 +21,18 @@ import Parser.First
 --      -> ] 
 ---------------------------
 
+--------------------------- CASE B
+-- Term -> [ Exp
+--      -> ]
+-- Exp  -> ( ) Term
+---------------------------
+
+
 
 spec :: Spec
 spec = do
   describe "createFirstMap" $ do
-    it "Should create a First Mapping for case A" $ createFirstMap (Data.Set.fromList [ProductionRule (NonTerminal "Term", [Term (Terminal "["), NonTerm (NonTerminal "Term")]),  ProductionRule (NonTerminal "Term" , [Term (Terminal "]")])]) (Data.Set.fromList [Terminal "[", Terminal "]"]) 
+    it "Should create a First Mapping for case A" $ do createFirstMap (Data.Set.fromList [ProductionRule (NonTerminal "Term", [Term (Terminal "["), NonTerm (NonTerminal "Term")]),  ProductionRule (NonTerminal "Term" , [Term (Terminal "]")])]) (Data.Set.fromList [Terminal "[", Terminal "]"]) 
       `shouldBe`
       (First (Data.Map.fromList [(Term (Terminal "EOF"),Data.Set.fromList [Terminal "EOF"]),(Term (Terminal "["),Data.Set.fromList [Terminal "["]),(Term (Terminal "]"), Data.Set.fromList [Terminal "]"]),(Term (Terminal "\948"),Data.Set.fromList [Terminal "\948"]),(NonTerm (NonTerminal "Term"),Data.Set.fromList [Terminal "[",Terminal "]",Terminal "\948"])]))
-
+    it "Should create a First Mapping for case B" $ do createFirstMap (Data.Set.fromList [ProductionRule (NonTerminal "Term", [Term (Terminal "["), NonTerm (NonTerminal "Exp")]),  ProductionRule (NonTerminal "Term" , [Term (Terminal "]")]), ProductionRule (NonTerminal "Exp", [Term (Terminal "("), Term (Terminal ")"), NonTerm (NonTerminal "Term")])]) (Data.Set.fromList [Terminal "[", Terminal "]", Terminal "(", Terminal ")"]) `shouldBe` First (Data.Map.fromList [(Term (Terminal "("), Data.Set.fromList [Terminal "("]),(Term (Terminal ")"),Data.Set.fromList [Terminal ")"]),(Term (Terminal "EOF"),Data.Set.fromList [Terminal "EOF"]),(Term (Terminal "["),Data.Set.fromList [Terminal "["]),(Term (Terminal "]"), Data.Set.fromList [Terminal "]"]),(Term (Terminal "\948"),Data.Set.fromList [Terminal "\948"]),(NonTerm (NonTerminal "Exp"),Data.Set.fromList [Terminal "("]),(NonTerm (NonTerminal "Term"), Data.Set.fromList [Terminal "[",Terminal "]",Terminal "\948"])])
