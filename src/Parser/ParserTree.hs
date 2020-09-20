0b26185399221053f76b988e1265883eb417e6fc
sdrafahl <shanedrafahl@gmail.com>
2020-09-19 19:52:49 -0500

    Adding the required data types

---
+++ src/Parser/ParserTree.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Parser.ParserTree where
+
+import Data.Set
+import Data.List
+import Data.Map
+import Debug.Trace
+import Parser.CFG
+
+data ParserTree = ParseTreeNode NonTerminal [ParserTree] | Leafe Terminal | EmptyTree deriving (Eq, Ord, Show)
+data ParseTreeStack = ParseTreeStack [ParserTree] deriving (Eq, Ord, Show)
+
+createLeafe :: Terminal -> ParserTree
+createLeafe term = Leafe term
+
+pushparseTreeStack :: ParseTreeStack -> ParserTree -> ParseTreeStack
+pushparseTreeStack (ParseTreeStack lst) tre = (ParseTreeStack (lst ++ [tre]))
+
+popnStack :: ParseTreeStack -> Int -> (ParseTreeStack, [ParserTree])
+popnStack (ParseTreeStack lsty) n =
+  let (amountToPop :: Int) = Data.List.length lsty - n
+      (remainderStack :: [ParserTree], r) = Data.List.splitAt amountToPop lsty
+  in  ((ParseTreeStack remainderStack), r)
+
+addParentToParseTree :: NonTerminal -> ParseTreeStack -> Int -> (ParserTree, ParseTreeStack)
+addParentToParseTree nonTerm treeStack reduceLength =
+  let (remainingStack :: ParseTreeStack, removed :: [ParserTree]) = popnStack treeStack reduceLength
+      (newTree :: ParserTree) = (ParseTreeNode nonTerm removed)
+  in  (newTree, remainingStack)
+
+pushTree :: ParseTreeStack -> ParserTree -> ParseTreeStack
+pushTree (ParseTreeStack st) tr = (ParseTreeStack (st ++ [tr]))
+
+createTreeFromStack :: ParseTreeStack -> ParserTree
+createTreeFromStack  (ParseTreeStack [a]) = a
+createTreeFromStack  (ParseTreeStack []) = EmptyTree
+createTreeFromStack  (ParseTreeStack (x:_)) = x
+
